3ad658dff6f05713de5f1adc5a0ab50c25c4f380
kenjiinthehouse <69293355+kenjiinthehouse@users.noreply.github.com>
2020-09-04 14:09:24 +0800

    SQL產生假資料

---
+++ mainproject/forum/產生假資料.php
@@ -0,0 +1,52 @@
+<?php
+
+// cast( floor(rand()*100) as int)
+
+
+/*
+
+INSERT 
+INTO `forum` (`sid`, `member_id`, `content`, `add_points`, `lose_points`, `accuse_points`, `post_time`) 
+VALUES (NULL, cast( floor(rand()*100000000) as int), '你好電池賓館上面，客服本來眼前加入時間桌面失去允許參考註冊時間思考儀器，認真按照很少下列警告改革照顧那麼多車輛招商透露允許回到，那樣成立難得權威面議問道，四周日韓，甚至強化告訴失敗行為龍岡長時間改革現在，監管一個臨床，別的能否力道淡淡，獲取您的立場，心裡。', cast( floor(rand()*10000) as int), cast( floor(rand()*10000) as int) ,cast( floor(rand()*100) as int), NOW());
+INSERT 
+INTO `forum` (`sid`, `member_id`, `content`, `add_points`, `lose_points`, `accuse_points`, `post_time`) 
+VALUES (NULL, cast( floor(rand()*100000000) as int), '回過神才發現，思考資料庫的存在意義，已讓我廢寢忘食。', cast( floor(rand()*10000) as int), cast( floor(rand()*10000) as int) ,cast( floor(rand()*100) as int), NOW());
+INSERT 
+INTO `forum` (`sid`, `member_id`, `content`, `add_points`, `lose_points`, `accuse_points`, `post_time`) 
+VALUES (NULL, cast( floor(rand()*100000000) as int), '我們都很清楚，這是個嚴謹的議題。資料庫可以說是有著成為常識的趨勢。', cast( floor(rand()*10000) as int), cast( floor(rand()*10000) as int) ,cast( floor(rand()*100) as int), NOW());
+INSERT 
+INTO `forum` (`sid`, `member_id`, `content`, `add_points`, `lose_points`, `accuse_points`, `post_time`) 
+VALUES (NULL, cast( floor(rand()*100000000) as int), '劉易斯曾提出，不要把所有特別合意的希望都放在。', cast( floor(rand()*10000) as int), cast( floor(rand()*10000) as int) ,cast( floor(rand()*100) as int), NOW());
+INSERT 
+INTO `forum` (`sid`, `member_id`, `content`, `add_points`, `lose_points`, `accuse_points`, `post_time`) 
+VALUES (NULL, cast( floor(rand()*100000000) as int), '希望各位能用心體會這段話。動機，可以說是最單純的力量。總而言之，資料庫究竟是怎麼樣的存在，始終是個謎題。
+', cast( floor(rand()*10000) as int), cast( floor(rand()*10000) as int) ,cast( floor(rand()*100) as int), NOW());
+INSERT 
+INTO `forum` (`sid`, `member_id`, `content`, `add_points`, `lose_points`, `accuse_points`, `post_time`) 
+VALUES (NULL, cast( floor(rand()*100000000) as int), '  孟子說過一句經典的名言，枉己者，未有能直人者也。', cast( floor(rand()*10000) as int), cast( floor(rand()*10000) as int) ,cast( floor(rand()*100) as int), NOW());
+INSERT 
+INTO `forum` (`sid`, `member_id`, `content`, `add_points`, `lose_points`, `accuse_points`, `post_time`) 
+VALUES (NULL, cast( floor(rand()*100000000) as int), '資料庫的出現仍然代表了一定的意義。我們要從本質思考，從根本解決問題。  ', cast( floor(rand()*10000) as int), cast( floor(rand()*10000) as int) ,cast( floor(rand()*100) as int), NOW());
+INSERT 
+INTO `forum` (`sid`, `member_id`, `content`, `add_points`, `lose_points`, `accuse_points`, `post_time`) 
+VALUES (NULL, cast( floor(rand()*100000000) as int), '突破和超越。這句話語雖然很短，但令我浮想聯翩。帶著這些問題，我們一起來審視資料庫。', cast( floor(rand()*10000) as int), cast( floor(rand()*10000) as int) ,cast( floor(rand()*100) as int), NOW());
+INSERT 
+INTO `forum` (`sid`, `member_id`, `content`, `add_points`, `lose_points`, `accuse_points`, `post_time`) 
+VALUES (NULL, cast( floor(rand()*100000000) as int), '這啟發了我。恩格斯說過，社會一旦有技術上的需要，則這種需要就會比十所大學更能把科學推向前進。', cast( floor(rand()*10000) as int), cast( floor(rand()*10000) as int) ,cast( floor(rand()*100) as int), NOW());
+INSERT 
+INTO `forum` (`sid`, `member_id`, `content`, `add_points`, `lose_points`, `accuse_points`, `post_time`) 
+VALUES (NULL, cast( floor(rand()*100000000) as int), '這段話讓我所有的疑惑頓時豁然開朗。', cast( floor(rand()*10000) as int), cast( floor(rand()*10000) as int) ,cast( floor(rand()*100) as int), NOW());
+INSERT 
+INTO `forum` (`sid`, `member_id`, `content`, `add_points`, `lose_points`, `accuse_points`, `post_time`) 
+VALUES (NULL, cast( floor(rand()*100000000) as int), '我們需要淘汰舊有的觀念，若沒有資料庫的存在，那麼後果可想而知。', cast( floor(rand()*10000) as int), cast( floor(rand()*10000) as int) ,cast( floor(rand()*100) as int), NOW());
+INSERT 
+INTO `forum` (`sid`, `member_id`, `content`, `add_points`, `lose_points`, `accuse_points`, `post_time`) 
+VALUES (NULL, cast( floor(rand()*100000000) as int), '在這種困難的抉擇下，本人思來想去，寢食難安。', cast( floor(rand()*10000) as int), cast( floor(rand()*10000) as int) ,cast( floor(rand()*100) as int), NOW());
+INSERT 
+INTO `forum` (`sid`, `member_id`, `content`, `add_points`, `lose_points`, `accuse_points`, `post_time`) 
+VALUES (NULL, cast( floor(rand()*100000000) as int), '車爾尼雪夫斯基告訴我們，為人粗魯意味著忘卻了自己的尊嚴。這段話令我陷入了沈思。', cast( floor(rand()*10000) as int), cast( floor(rand()*10000) as int) ,cast( floor(rand()*100) as int), NOW());
+
+
+
+
+*/
